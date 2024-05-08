982c58d37affefeee42611c387ae6cf87c913089
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2024-05-08 13:28:30 +0700

    FIX: delete finished btn for awhile

--- components/blocks/footerApp.tsx
+++ components/blocks/footerApp.tsx
@@ -41,14 +41,14 @@ const FooterApp: FC<Props> = (props: Props) => {
   return (
     <footer className={props.className}>
       <nav>
-        <ul className="flex items-center justify-between p-4">
-          <li className="hidden w-1/3 justify-start sm:flex">
+        <ul className="flex items-center justify-center p-4">
+          {/* <li className="hidden w-1/3 justify-start sm:flex">
             <h6 className="text-xs">Fokusify ver 0.35</h6>
-          </li>
+          </li> */}
           <li className="flex flex-1 justify-center">
             <Toolbar />
           </li>
-          <li className="flex w-1/3 justify-end">
+          {/* <li className="flex w-1/3 justify-end">
             <Button
               variant={style === "LIGHT" ? "secondary" : "default"}
               disabled={isPending}
@@ -56,7 +56,7 @@ const FooterApp: FC<Props> = (props: Props) => {
             >
               Finish
             </Button>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </footer>
