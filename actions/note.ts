bae1c80bb45e147d941122286f0d7ddff3e6d55e
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2024-05-17 01:49:44 +0700

    DELETE: Create note server actions

--- actions/note.ts
+++ actions/note.ts
@@ -2,10 +2,6 @@
 
 import { Note } from "@/lib/db/data/note";
 
-export const create = async (fileId: string, value: string) => {
-  return Note.create({ fileId, value });
-};
-
 export const deleteNote = async (noteId: string) => {
   return Note.delete(noteId);
 };
